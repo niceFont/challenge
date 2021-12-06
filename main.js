a6815885091a128fb6cdef799b4a3ceb036f898b
niceFont <peterwilms@protonmail.com>
2021-12-06 21:42:32 +0000

    cleanup and some additional styling

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ class Dialog {
   _visible = false
   _dialog = null
   _backdrop = null
-
+  _button = null
   _subscribers = []
 
   constructor(message) {
     this.message = message
-    this.render()
+    this._render()
   }
   
   _createButton() {
@@ -17,7 +17,7 @@ class Dialog {
     button.addEventListener("click", () => {
       this.show()
     })
-    return button
+    this._button = button
   }
   
   _createDialog() {
@@ -34,36 +34,44 @@ class Dialog {
     const dialogText = document.createTextNode(this.message)
     span.appendChild(dialogText)
     
+    const buttonContainer = document.createElement("div")
     const yesButton = document.createElement("button")
     yesButton.textContent = "Yes"
     yesButton.addEventListener("click", () => {
-      this.emit(true)
+      this._notify(true)
       this.hide()
     })
+    yesButton.classList.add("dialog__actionbuttons")
+    yesButton.classList.add("yes")
     const cancelButton = document.createElement("button")
     cancelButton.textContent = "Cancel"
     cancelButton.addEventListener("click", () => {
-      this.emit(false)
+      this._notify(false)
       this.hide()
     })
+    cancelButton.classList.add("dialog__actionbuttons")
+    cancelButton.classList.add("cancel")
+    buttonContainer.appendChild(yesButton)
+    buttonContainer.appendChild(cancelButton)
 
     container.appendChild(span)
-    container.appendChild(yesButton)
-    container.appendChild(cancelButton)
+    container.appendChild(buttonContainer)
 
     this._dialog.appendChild(container)
-
   }
-  render() {
+  _render() {
   
-    const button = this._createButton()
+    this._createButton()
     this._createDialog()
     const app = document.getElementById("app")
-    app.appendChild(button)
+    app.appendChild(this._button)
     app.appendChild(this._dialog)
     app.appendChild(this._backdrop)
   }
   
+  /**
+   * Hide dialog
+   */
   show() {
     this._visible = true
     this._dialog.classList.add("dialog--active")
@@ -71,28 +79,36 @@ class Dialog {
 
   }
 
+  /**
+   * Show dialog
+   */
   hide() {
     this._visible = false
     this._dialog.classList.remove("dialog--active")
     this._backdrop.classList.remove("dialog__backdrop--active")
   }
   
-  emit(value) {
+  _notify(value) {
     this._subscribers.forEach((cb) => {
       cb(value)
     })
   }
   
+  /**
+   *  Subscribe to event using callback
+   *  Returns unsubscribe function
+   */
   subscribe(cb) {
     this._subscribers.push(cb)
+
+    return () => {
+      const i = this._subscribers.findIndex((subs) => subs === cb)
+      this._subscribers.splice(i, 1)
+    }
   }
 }
 
 window.onload = function () {
-
-  
-  
-  
   const d = new Dialog("Hello World")
   
   const node = document.createElement("span")
