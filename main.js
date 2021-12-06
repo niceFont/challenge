0e7f6b9e636b0284717d4848c80ae8a2ad8024ac
niceFont <peterwilms@protonmail.com>
2021-12-06 21:08:00 +0000

    backdrop fixed and notify added

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 class Dialog {
   _visible = false
   _dialog = null
+  _backdrop = null
+
+  _subscribers = []
+
   constructor(message) {
     this.message = message
     this.render()
@@ -20,6 +24,9 @@ class Dialog {
     this._dialog = document.createElement("div")
     this._dialog.classList.add("dialog")
 
+    this._backdrop = document.createElement("div")
+    this._backdrop.classList.add("dialog__backdrop")
+
     const container = document.createElement("div")
     container.classList.add("dialog__container")
 
@@ -29,9 +36,17 @@ class Dialog {
     
     const yesButton = document.createElement("button")
     yesButton.textContent = "Yes"
+    yesButton.addEventListener("click", () => {
+      this.emit(true)
+      this.hide()
+    })
     const cancelButton = document.createElement("button")
     cancelButton.textContent = "Cancel"
-    
+    cancelButton.addEventListener("click", () => {
+      this.emit(false)
+      this.hide()
+    })
+
     container.appendChild(span)
     container.appendChild(yesButton)
     container.appendChild(cancelButton)
@@ -46,19 +61,31 @@ class Dialog {
     const app = document.getElementById("app")
     app.appendChild(button)
     app.appendChild(this._dialog)
+    app.appendChild(this._backdrop)
   }
   
   show() {
     this._visible = true
     this._dialog.classList.add("dialog--active")
+    this._backdrop.classList.add("dialog__backdrop--active")
 
   }
 
   hide() {
     this._visible = false
+    this._dialog.classList.remove("dialog--active")
+    this._backdrop.classList.remove("dialog__backdrop--active")
   }
   
+  emit(value) {
+    this._subscribers.forEach((cb) => {
+      cb(value)
+    })
+  }
   
+  subscribe(cb) {
+    this._subscribers.push(cb)
+  }
 }
 
 window.onload = function () {
@@ -67,4 +94,12 @@ window.onload = function () {
   
   
   const d = new Dialog("Hello World")
+  
+  const node = document.createElement("span")
+  const text = document.createTextNode("")
+  node.appendChild(text)
+  document.body.appendChild(node)
+  d.subscribe((value) => {
+    node.innerText = `You have clicked ${value ? 'Yes' : 'Cancel'}`
+  })
 }
